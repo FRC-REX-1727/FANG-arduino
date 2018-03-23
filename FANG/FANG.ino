916b7cbf76a8ef1b7d77622a7e1958dbe9e85fb6
Anton Pozharskiy <apozharski@gmail.com>
2018-03-23 10:23:49 -0400

    added library reference. move printing to loop function

--- FANG/FANG.ino
+++ FANG/FANG.ino
@@ -10,15 +10,17 @@ int counter = 0;
 float vel = 0;
 const float DISTANCE_PER_PULSE = 2*RADIUS*PI/RESOLUTION;
 unsigned long last_pulse = 0;
+float dummy = 10;
 
 void setup() {
-  Serial.begin(57600);
+  Serial.begin(9600);
   last_pulse = millis();
   attachInterrupt(0, rotate, CHANGE);
   attachInterrupt(1, rotate, CHANGE);
 }
 
 void loop() {
+  Serial.print(counter);Serial.print("\t");Serial.println(vel);
 }
 
 // rotate is called anytime the rotary inputs change state.
@@ -29,12 +31,10 @@ void rotate() {
     counter++;
     vel = DISTANCE_PER_PULSE/(curr_pulse-last_pulse);
     last_pulse = curr_pulse;
-    Serial.println(counter);
   } else if (result == DIR_CCW) {
     counter--;
     vel = DISTANCE_PER_PULSE/(curr_pulse-last_pulse);
     last_pulse = curr_pulse;
-    Serial.println(counter);
   }
 }
 
--- README.md
+++ README.md
@@ -7,3 +7,6 @@ an FRC legal input sensor.
 ## Current State
 This repository is currently used purely as a _prototyping_ branch and contains the main arduino code 
 to be used on the Arduino MINI PRO.
+
+## Dependencies
+This velocity controller uses the Ben Buxton Quadrature Encoder library which can be found [here](https://github.com/buxtronix/arduino/tree/master/libraries/Rotary).
